1cc7031eb148ad96949113fa30111d96ca20466d
Jasmine Garcia <jasmine.garcia@sliqpay.com>
2026-02-22 12:44:40 +0800

    feat: init login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [InputGroupModule, InputGroupAddonModule, InputTextModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  standalone: true
 })
 export class LoginComponent {
-
+  user = {
+    email: '',
+    password: ''
+  };
 }
