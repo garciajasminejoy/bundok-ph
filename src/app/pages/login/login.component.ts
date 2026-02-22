7778a28eec6f03e2cb772726f7c588ed59db3617
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-02-22 14:08:09 +0800

    feat: login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,17 +3,26 @@ import { FormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-login',
-  imports: [InputGroupModule, InputGroupAddonModule, InputTextModule, FormsModule],
+  imports: [
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    FormsModule,
+    ButtonModule,
+    PanelModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  standalone: true
+  standalone: true,
 })
 export class LoginComponent {
   user = {
     email: '',
-    password: ''
+    password: '',
   };
 }
