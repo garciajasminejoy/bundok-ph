73db4a871c2799d51e2e7b7fc311599558cbcd3f
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-02-22 12:45:28 +0800

    feat: hikes component

---
+++ src/app/pages/hikes/hikes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hikes',
+  imports: [],
+  templateUrl: './hikes.component.html',
+  styleUrl: './hikes.component.css'
+})
+export class HikesComponent {
+
+}
