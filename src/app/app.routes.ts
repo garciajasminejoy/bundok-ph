8df0476fd8d57b695570b6df4f4f6de26a0c00cd
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-02-24 21:37:53 +0800

    feat: redirect to all hikes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HikesComponent } from './pages/hikes/hikes.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,13 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'hikes',
+        component: HikesComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
     }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -25,4 +26,10 @@ export class LoginComponent {
     email: '',
     password: '',
   };
+
+  router = inject(Router);
+
+  login(): void {
+    this.router.navigate(['/hikes']);
+  }
 }
