0d2e56a6e047f6eaa45d2dfd85e2e26efa428722
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-02-24 22:38:52 +0800

    feat: initial filters

--- src/app/pages/main/pages/hikes/hikes.component.ts
+++ src/app/pages/main/pages/hikes/hikes.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-hikes',
-  imports: [],
+  imports: [SelectModule, FormsModule],
   templateUrl: './hikes.component.html',
   styleUrl: './hikes.component.css',
   standalone: true
 })
 export class HikesComponent {
+  selectedLocation?: string;
+  selectedDifficulty?: string;
 
+  locations = [
+    { name: 'Cordillera' },
+    { name: 'Rizal' },
+    { name: 'Batangas' },
+    { name: 'Mindoro' },
+    { name: 'Nueva Vizcaya' },
+  ];
+  difficultyLevels = [
+    { name: 'Easy' },
+    { name: 'Moderate' },
+    { name: 'Hard' },
+  ];
 }
