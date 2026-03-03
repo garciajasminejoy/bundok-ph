d15a1cf54f8e887d5e5616110d9aede95d71efbe
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-03-03 23:58:02 +0800

    feat: book button

--- src/app/pages/main/pages/hike-details/hike-details.component.ts
+++ src/app/pages/main/pages/hike-details/hike-details.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute, provideRouter, RouterModule } from '@angular/router';
 import { hikeDetailsRoutes } from './hike-details.routes';
 import { HikesService } from '../../../../services/hikes.service';
 import { TagModule } from 'primeng/tag';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-hike-details',
-  imports: [RouterModule, TagModule],
+  imports: [RouterModule, TagModule, ButtonModule],
   templateUrl: './hike-details.component.html',
   styleUrl: './hike-details.component.css',
 })
