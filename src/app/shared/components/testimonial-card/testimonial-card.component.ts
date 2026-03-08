022542c6af434acbab242f1e43a14314b2a0dd58
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-03-08 21:49:47 +0800

    feat: community section

--- src/app/shared/components/testimonial-card/testimonial-card.component.ts
+++ src/app/shared/components/testimonial-card/testimonial-card.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-testimonial-card',
-  imports: [],
+  imports: [PanelModule],
   templateUrl: './testimonial-card.component.html',
   styleUrl: './testimonial-card.component.css'
 })
