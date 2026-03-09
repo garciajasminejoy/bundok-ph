7182e33e59a04b53a9cbbeaacc3db4c8df7e47e4
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-03-09 21:02:01 +0800

    feat: add hikes section in trail detail page

--- src/app/pages/main/pages/trail-details/trail-details.component.ts
+++ src/app/pages/main/pages/trail-details/trail-details.component.ts
@@ -2,23 +2,28 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TrailsService } from '../../../../services/trails/trails.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { TestimonialCardComponent } from '../../../../shared/components/testimonial-card/testimonial-card.component';
+import { HikeCardComponent } from '../../../../shared/components/hike-card/hike-card.component';
+import { HikesService } from '../../../../services/hikes/hikes.service';
 
 @Component({
   selector: 'app-trail-details',
-  imports: [RouterModule, TestimonialCardComponent],
+  imports: [RouterModule, TestimonialCardComponent, HikeCardComponent],
   templateUrl: './trail-details.component.html',
   styleUrl: './trail-details.component.css',
 })
 export class TrailDetailsComponent implements OnInit {
-  moutainsService = inject(TrailsService);
+  trailsService = inject(TrailsService);
+  hikesService = inject(HikesService);
 
   activatedRoute = inject(ActivatedRoute);
 
-  mountain = this.moutainsService.getTrailById(
+  trail = this.trailsService.getTrailById(
     Number(this.activatedRoute.snapshot.params['id']),
   );
 
-  ngOnInit(): void {
-    console.log(this.mountain);
-  }
+  hikes = this.hikesService
+    .hikes()
+    .filter((hike) => hike.trailId === this.trail?.id);
+
+  ngOnInit(): void {}
 }
