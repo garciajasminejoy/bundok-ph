e863aed9584f026579e569fd5e2ea77421c4bb7a
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-03-10 22:39:42 +0800

    feat: organizers page filters

--- src/app/pages/main/pages/organizers/organizers.component.ts
+++ src/app/pages/main/pages/organizers/organizers.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-organizers',
-  imports: [],
+  imports: [
+    SelectModule,
+    ButtonModule,
+    InputGroupModule,
+    FormsModule,
+    InputGroupAddonModule,
+    InputTextModule
+  ],
   templateUrl: './organizers.component.html',
   styleUrl: './organizers.component.css',
-  standalone: true
+  standalone: true,
 })
 export class OrganizersComponent {
-
+  locations = [
+    { name: 'Cordillera' },
+    { name: 'Rizal' },
+    { name: 'Batangas' },
+    { name: 'Mindoro' },
+    { name: 'Nueva Vizcaya' },
+  ];
+  selectedLocation?: string;
 }
