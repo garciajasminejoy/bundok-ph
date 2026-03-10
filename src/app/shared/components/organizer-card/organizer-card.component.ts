0996584d1288110bb911149fda662d23e4dee0a9
Jasmine Garcia <garciajasminejoy@gmail.com>
2026-03-10 22:51:10 +0800

    feat: organizer card

---
+++ src/app/shared/components/organizer-card/organizer-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer-card',
+  imports: [],
+  templateUrl: './organizer-card.component.html',
+  styleUrl: './organizer-card.component.css'
+})
+export class OrganizerCardComponent {
+
+}
